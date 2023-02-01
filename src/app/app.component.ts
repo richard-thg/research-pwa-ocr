7cd07e1947dee304dcb90f1bb7ecdd5fc906cf30
Richard Omoka <richard.omoka@gmail.com>
2023-02-01 23:54:32 +0300

    Can read logbook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+interface IKeyValue {
+  key: string;
+  value?: any;
+}
+
 interface IOcrWord {
   boundingBox: number[];
   text: string;
@@ -45,6 +50,7 @@ export class AppComponent {
   documentTypeLogBook: string = 'LOGBOOK_DOCUMENT';
   documentTypeKraPin: string = 'KRA_PIN_DOCUMENT';
   documentTypeNA: string = 'NA';
+  logbookDetails: IKeyValue[] = [];
 
   processOcrResult($event: string) {
     let ocrResult: IOcrResult = JSON.parse($event);
@@ -56,10 +62,16 @@ export class AppComponent {
     switch (firstLine) {
       case logbookFistLinetext:
         this.documentType = 'LOGBOOK_DOCUMENT';
+        if (lines) {
+          this.readLogbook(lines);
+        }
         break;
 
       case kraPinFistLinetext:
         this.documentType = 'KRA_PIN_DOCUMENT';
+        if (lines) {
+          this.readKraPin(lines);
+        }
         break;
 
       default:
@@ -67,4 +79,114 @@ export class AppComponent {
         break;
     }
   }
+
+  readLogbook(lines: IOcrLine[]) {
+    let logbookNumber = lines.at(1)?.text;
+    this.pushLogbookDetails('Logbook Number', logbookNumber);
+
+    let entryNumber = lines.at(3)?.words.at(2)?.text;
+    this.pushLogbookDetails('Entry Number', entryNumber);
+
+    let originalNumber = lines.at(7)?.words.at(2)?.text;
+    this.pushLogbookDetails('Original Number', originalNumber);
+
+    let registration = this.getNextValue(lines, 'Registration:');
+    this.pushLogbookDetails('Registration', registration);
+
+    let passengers = this.getNextValue(lines, 'Passengers:');
+    this.pushLogbookDetails('Passengers', passengers);
+
+    let chassisFrame = this.getNextValueContains(lines, 'Chassis/Frame:', 1);
+    this.pushLogbookDetails('Chassis/Frame', chassisFrame);
+
+    let tareWeight = this.getNextValue(lines, 'Tare Weight:');
+    this.pushLogbookDetails('Tare Weight', tareWeight);
+
+    let make = this.getNextValue(lines, 'Make:');
+    this.pushLogbookDetails('Make', make);
+
+    let taxClass = this.getNextValue(lines, 'Tax class:');
+    this.pushLogbookDetails('Tax class', taxClass);
+
+    let vmodel = this.getNextValue(lines, 'Model:');
+    this.pushLogbookDetails('Model', vmodel);
+
+    let axles = this.getNextValue(lines, 'Axles:');
+    this.pushLogbookDetails('Axles', axles);
+
+    let vtype = this.getNextValue(lines, 'Type:');
+    this.pushLogbookDetails('Type', vtype);
+
+    let loadCapacity = this.getNextValue(lines, 'Load Capacity(Kg):');
+    this.pushLogbookDetails('Load Capacity(Kg)', loadCapacity);
+
+    let vbody = this.getNextValue(lines, 'Body:');
+    this.pushLogbookDetails('Body', vbody);
+
+    let previousReg = this.getNextValue(lines, 'Previous Reg.', 'Fuel:');
+    this.pushLogbookDetails('Previous Reg', previousReg);
+
+    let fuel = this.getNextValue(lines, 'Fuel:');
+    this.pushLogbookDetails('Fuel', fuel);
+
+    let country = this.getNextValue(lines, 'country:', 'Man Year:');
+    this.pushLogbookDetails('country', country);
+
+    let manYear = this.getNextValue(lines, 'Man Year:');
+    this.pushLogbookDetails('Man Year', manYear);
+
+    let rating = this.getNextValue(lines, 'Rating:');
+    this.pushLogbookDetails('Rating', rating);
+
+    let engineNo = this.getNextValue(lines, 'Engine No:');
+    this.pushLogbookDetails('Engine No', engineNo);
+
+    let color = this.getNextValue(lines, 'Color:');
+    this.pushLogbookDetails('Color', color);
+
+    let regDate = this.getNextValue(lines, 'Reg. Date:');
+    this.pushLogbookDetails('Reg. Date', regDate);
+
+    let grossWeight = this.getNextValue(lines, 'Gross weight:');
+    this.pushLogbookDetails('Gross weight', grossWeight);
+
+    let duty = this.getNextValue(lines, 'Duty:');
+    this.pushLogbookDetails('Duty', duty);
+
+    let previousOwners = this.getNextValueContains(
+      lines,
+      'Number of previous owners:',
+      3
+    );
+    this.pushLogbookDetails('Number of previous owners', previousOwners);
+  }
+
+  pushLogbookDetails(key: string, value: any) {
+    this.logbookDetails.push({ key: key, value: value });
+  }
+
+  getNextValue(lines: IOcrLine[], searchTerm: string, nextValue?: string) {
+    let itemIndex = lines.findIndex((aLine) => {
+      return aLine.text === searchTerm;
+    });
+    let s = lines.at(itemIndex + 1)?.text;
+    if (s === nextValue) {
+      return '';
+    }
+    return s;
+  }
+
+  getNextValueContains(lines: IOcrLine[], searchTerm: string, index: number) {
+    let item = lines.filter((aLine) => {
+      return aLine.text.includes(searchTerm);
+    })[0];
+    let itemIndex = lines.indexOf(item);
+    let s = lines.at(itemIndex)?.words.at(index)?.text;
+    if (s?.includes(':')) {
+      return s.split(':')[1];
+    }
+    return s;
+  }
+
+  readKraPin(lines: IOcrLine[]) {}
 }
